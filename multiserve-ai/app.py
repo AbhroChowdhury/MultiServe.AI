08107345d4f939a72fd0e502c05a4f4719f0fb47
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-08-15 00:14:26 -0600

    connecting FE and BE

--- multiserve-ai/app.py
+++ multiserve-ai/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello from Flask!'
+
+@app.route('/api/data')
+def get_data():
+    return {'message': 'Data from Flask!'}
--- multiserve-ai/src/App.js
+++ multiserve-ai/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios.get('/api/data') 
+      .then(response => {
+        setData(response.data.message); 
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data from Flask:</h1>
+      <p>{data}</p>
     </div>
   );
 }
